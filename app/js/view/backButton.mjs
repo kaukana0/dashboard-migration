75adbc0f58e4252d537789aace4ff82e6cf0b49f
robert <kaukana0@tuta.io>
2023-12-07 11:20:29 +0100

    addendum

--- app/js/view/backButton.mjs
+++ app/js/view/backButton.mjs
@@ -1,12 +1,12 @@
 
 export function show() {
   const b = document.getElementById("globalBackButton")
-  b.style.setProperty('--gbbDisplay', "block")
+  b.style.setProperty('--gbbdisplay', "block")
 }
 
 export function hide() {
   const b = document.getElementById("globalBackButton")
-  b.style.setProperty('--gbbDisplay', "none")
+  b.style.setProperty('--gbbdisplay', "none")
 }
 
 export function callback(cb) {
