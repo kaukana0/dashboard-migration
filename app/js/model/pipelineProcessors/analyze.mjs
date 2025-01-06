4873bba13b51737cedbd1532684c1473e80b9168
robert <kaukana0@tuta.io>
2025-01-06 15:30:03 +0100

    #DASHBOARD-467 show "no data available"

--- app/js/model/pipelineProcessors/analyze.mjs
+++ app/js/model/pipelineProcessors/analyze.mjs
@@ -1,8 +1,9 @@
 // useful during development and configuring
+// note: "input" and "output" is meant from the pipeline's POV after it ran all processors
 export function process(inputData,c,output) {
-  console.debug("inputData:", inputData)
-  console.debug("data from cfg:", c)
-  console.debug("output:", output)
+  console.debug("input data from REST request:", inputData)
+  console.debug("input data from cfg:", c)
+  console.debug("pipeline's output data:", output)
 
   let txt = ""
   for(let i=0;i<inputData.size.length;i++) {
